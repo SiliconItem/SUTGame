5da204b3167fe5bdb1c805d79b73e4236eca11e7
developer <dev@lop.er>
2021-01-12 17:16:12 +0300

    Composite modal window (https://itchief.ru/javascript/modal-window)

--- src/main/resources/static/js/gamepage.js
+++ src/main/resources/static/js/gamepage.js
@@ -1,4 +1,3 @@
-
 var stompClient = null;
 function setConnected(connected) {
 
@@ -27,17 +26,14 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendMessage(cid) {
-    stompClient.send("/app/event", {}, JSON.stringify({'commandName':'tabulator', 'target':cid}));
+function sendMessage(field, cid) {
+    stompClient.send("/app/event", {}, JSON.stringify({'commandName':field, 'target':cid}));
 }
 
 function infoge2() {
     console.log("INFO GATHERING");
     alert("INFO GETHERING");
 }
-
-
-
 
 
 btn.onclick = function () {
@@ -61,15 +57,19 @@ function showMessageOutput(messageOutput) {
     var color = messageOutput.color;
     var audio = new Audio("/media/s3.mp3");
 
-    var modal = document.getElementById("my_modal");
-    var btn = document.getElementById("btn_modal_window");
-    var span = document.getElementsByClassName("close_modal_window")[0];
-
-
-    modal.style.display = "block";
-    span.onclick = function () {
-        modal.style.display = "none";
+    var qAction = messageOutput.action;
+    if (qAction) {
+        var modal = $modal({
+            title: 'Текст заголовка',
+            content: '<p>Содержимое модального окна...</p>',
+            footerButtons: [
+                { class: 'btn btn__cancel', text: 'Отмена', handler: 'modalHandlerCancel' },
+                { class: 'btn btn__ok', text: 'ОК', handler: 'modalHandlerOk' }
+            ]
+        });
+        modal.show();
     }
+
 
     document.getElementById(cid).style.backgroundColor  = color;
     document.getElementById(cid).style.opacity = '0.3';
